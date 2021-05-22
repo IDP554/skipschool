4a2a3838d1e4f22abd41e053f202bd52d848c4ac
李大炮 <zf_554@icloud.com>
2021-05-22 16:04:33 +0800

    已经可以使用

--- js/content-script.js
+++ js/content-script.js
@@ -1,17 +1,24 @@
-$(function() {
-	// 进入页面
-	console.log("进入自动刷课程序！");
-	// 获取课程ID的正则表达式
-	let wlan = /VideoCollectionID=(\d+)/
+// 等待 DOM 加载完毕
+ready(function(){
 	// 获取当前页面网址
 	let selfurl = window.location.href;
+	// 检测是否是刷课页面
+	let skipschool = /VideoCollectionID/
+	if (null == skipschool.exec(selfurl)) {
+		return;
+	}
+	// 进入页面
+	console.log("进入自动刷课程序！");
+	
+	// 获取课程ID的正则表达式
+	let wlan = /VideoCollectionID=(\d+)/
 	// 当前课程ID
-	let playID = wlan.exec(selfurl)[1];
+	let courseID = wlan.exec(selfurl)[1];
 	// 获取课程元素
-	let video = $("li.chapterConts");
+	let videos = $("li.chapterConts");
 	// 保存未刷课程信息
-	let infos = [];
-	for (let v of video) {
+	var infos = [];
+	for (let v of videos) {
 		let href = $(v).children("a").attr("href");
 		let url = "https://hnwledu.ls365.net" + href.trim();
 		let id = wlan.exec(href)[1];
@@ -26,45 +33,86 @@ $(function() {
 			infos.push(obj);
 		}
 	}
-	// 获取当前需要刷的课
-	let play = infos[0]
-	if(playID !== play.id) {
-		self.location.href=play.url
+
+	if(infos.length == 0) {
+		console.log("此学科已学完！");
+		alert("此学科已学完！");
+		return;
 	}
 
-	let auto = $("video")[0];
-	$(auto).prop('muted', true);
-	let outter = $("div.outter");
+	// 跳转到未刷完的课程页
+	let skip = infos[0]
+	if(courseID !== skip.id) {
+		self.location.href=skip.url
+	}
 
 	// 等待视频加载完毕
-	if($("video")[0].readyState=="complete"||$("video")[1].readyState=="loaded"){
-		console.log("视频已加载完毕");
+	let intervalID = setInterval(() => {
+		if($("video")[0].readyState==4) {
+			console.log("视频加载完毕");
+			clearInterval(intervalID);
+			autoplay();
+		}
+	}, 1000);
+
+
+
+
+	var autoplay = function(){
+		console.log("开始刷课...");
+
+		// 获取视频对象
+		let video = $("video")[0];
+
+		// 默认静音处理(谷歌66版本后想要视频自动播放需静音)
+		$(video).prop('muted', true);
+
+		// 判断课程ID是否对应
+		if(/rmd/.exec(selfurl) !== null) {
+			self.location.href=skip.url
+		}else{
+			$(video).trigger("play")
+			video.playbackRate = 1
+		}
+
+		// 监听视频是否播放
+		let reset = setInterval(() => {
+			if(video.paused) {
+				clearInterval(reset);
+				// 视频监听
+				monitor();
+			}
+		}, 1000);
 	}
 
-	// 延迟播放
-	setTimeout(function(){
-		console.log("延迟播放");
-		// 初始化加载
-		if(/rmd/.exec(selfurl) !== null) {
-			self.location.href=play.url
-		}else{
-			console.log("播放视频");
-			// $(outter[0]).trigger('click')
-			$(auto).trigger('play');
-			// $(auto).trigger('pause');
-			// $("video")[0].playbackRate = 2
-			// $("video")[1].playbackRate = 2
-			// $(auto).play()
+
+	var monitor = function() {
+		// 视频刷新重新赋值
+		let video = $("video")[0];
+		$(video).prop('muted', true);
+		if(video.paused) {
+			$(video).trigger("play")
 		}
-	},2000);
-	
-	// console.log($("video"));
-	// setInterval(function(){
-	// 	// 循环改变视频速率
-	// 	// $("video")[0].playbackRate = 5
-	// 	console.log("视频以加速处理");
-	// 	console.log($("video")[0]);
-	// 	console.log($("video")[1]);
-	// },10000)
-	
+
+
+		// 循环监听
+		let handle = setInterval(() => {
+			if(video.paused && $("div#reader_msgbg").css("display")=="none") {
+				let stop = confirm("视频被检测到已暂停！是否恢复播放！");
+				if(!stop) {
+					clearInterval(handle);
+				}else{
+					$(video).trigger("play")
+				}
+			}else if($("div#reader_msgbg").css("display")=="block"){
+				clearInterval(handle);
+				window.location.reload()
+			}else if(video.readyState==4) {
+				if(video.playbackRate < 10){
+					console.log("视频已加速至10倍速！");
+					video.playbackRate = 10
+				}
+			}
+		}, 1000);
+	}
 });
